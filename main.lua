190b303e3196af82f4b6f62d35a047da1bb30649
nikolasmurdock <nikolasmurdock@gmail.com>
2016-09-19 12:41:40 -0500

    Square around circle
    So, there´s a square around the circle.
    It´s a sandbox.

--- main.lua
+++ main.lua
@@ -26,24 +26,54 @@ function love.load()
 	o.ground.shape = p.newRectangleShape(width*2,50) --(width, height)
 	o.ground.fixture = p.newFixture(o.ground.body, o.ground.shape)--Attach shape to body
 
+	--Left wall
+	o.leftWall = {}
+	o.leftWall.body = p.newBody(world, 0, height/2, "static")--remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
+	o.leftWall.shape = p.newRectangleShape(50,height) --(width, height)
+	o.leftWall.fixture = p.newFixture(o.leftWall.body, o.leftWall.shape)--Attach shape to body
+
+	--Right wall
+	o.rightWall = {}
+	o.rightWall.body = p.newBody(world, width, height/2, "static")--remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
+	o.rightWall.shape = p.newRectangleShape(50,height) --(width, height)
+	o.rightWall.fixture = p.newFixture(o.rightWall.body, o.rightWall.shape)--Attach shape to body
+
+	--Ceiling
+	o.ceiling = {}
+	o.ceiling.body = p.newBody(world, width/2, 0, "static")--remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
+	o.ceiling.shape = p.newRectangleShape(width,50) --(width, height)
+	o.ceiling.fixture = p.newFixture(o.ceiling.body, o.ceiling.shape)--Attach shape to body
+
 	--Create the ball
 	o.ball = {}
 	o.ball.body = p.newBody(world, width/2, height/2, "dynamic")
 	o.ball.shape = p.newCircleShape(20)--We just declare the radius, because x, y, and the world have already been set by the body
 	o.ball.fixture = p.newFixture(o.ball.body, o.ball.shape) --attach body to shape with a density of one (body, shape, density)
-	o.ball.fixture:setRestitution(0.8) --let the ball bounce
+	o.ball.fixture:setRestitution(0.2) --let the ball bounce
+	o.ball.fixture:setFriction(0.9)
 
 	--initial graphics setup
-  g.setBackgroundColor(104, 136, 248) --set the background color to a nice blue
-  love.window.setMode(width, height) --set the window dimensions to 650 by 650 with no fullscreen, vsync on, and no antialiasing
+	g.setBackgroundColor(0, 0, 0) --set the background color to a nice blue
+	love.window.setMode(width, height) --set the window dimensions to 650 by 650 with no fullscreen, vsync on, and no antialiasing
 end
+
+function love.keyreleased(key)
+	if key == "space" then
+		o.ball.body:applyForce(0,-10000)
+	end
+
+	if key == "escape" then
+      love.event.quit()
+   end
+end
+
 
 function love.update(dt)
 	world:update(dt)--Update the world
 
-	if k.isDown("up") or k.isDown("w") then
+	--[[if love.keyreleased("up") or love.keyreleased("w") then
 		o.ball.body:applyForce(0, -300) 
-	end
+	end]]--
 
 	if k.isDown("down") or k.isDown("s") then
 		o.ball.body:applyForce(0, 300)
@@ -58,9 +88,12 @@ function love.update(dt)
 	end
 end
 function love.draw()
-  g.setColor(72, 160, 14) -- set the drawing color to green for the ground
-  g.polygon("fill", o.ground.body:getWorldPoints(o.ground.shape:getPoints())) -- draw a "filled in" polygon using the ground's coordinates
+	g.setColor(255, 255, 255) -- set the drawing color to green for the ground
+	g.polygon("fill", o.ground.body:getWorldPoints(o.ground.shape:getPoints())) -- draw a "filled in" polygon using the ground's coordinates
+	g.polygon("fill", o.leftWall.body:getWorldPoints(o.leftWall.shape:getPoints()))
+	g.polygon("fill", o.rightWall.body:getWorldPoints(o.rightWall.shape:getPoints()))
+	g.polygon("fill", o.ceiling.body:getWorldPoints(o.ceiling.shape:getPoints()))
 
-  g.setColor(193, 47, 14) --set the drawing color to red for the ball
-  g.circle("fill", o.ball.body:getX(), o.ball.body:getY(), o.ball.shape:getRadius())
+	g.setColor(255, 255, 255) --set the drawing color to red for the ball
+	g.circle("fill", o.ball.body:getX(), o.ball.body:getY(), o.ball.shape:getRadius(),9)
 end
